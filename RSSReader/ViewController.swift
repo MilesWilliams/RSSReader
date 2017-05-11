b535781bb56d1cdadafc3d25edb327721cef5df3
MilesWilliams <miles@klarity.co.za>
2017-05-11 22:58:25 +0200

    Init commit

---
+++ RSSReader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RSSReader
+//
+//  Created by MIles Work on 2017/05/11.
+//  Copyright © 2017 StudioRepublika. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
